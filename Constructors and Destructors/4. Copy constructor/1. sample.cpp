b26fe6d1d44090ff0c5e0ec51a86e1d70ce8594f
Asrasadik <47492644+Asrasadik@users.noreply.github.com>
2020-08-07 23:16:31 +0530

    adding copy constructor

---
+++ Constructors and Destructors/4. Copy constructor/1. sample.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+class PrepInsta{
+	private:
+		int x,y;
+	public:
+		//parameterised constructor
+		PrepInsta(int x1, int y1){
+			x=x1;
+			y=y1;
+			cout<<"parameterised constructor is called"<<endl;
+		}
+		//copy constructor
+		PrepInsta(const PrepInsta &p1){
+			x=p1.x;
+			y=p1.y;
+			cout<<"copy constructor is called"<<endl;
+		}
+		int getX(){
+			return x;
+		}
+		int gety(){
+			return y;
+		}
+};
+
+int main(){
+	//parameterised constructor
+	PrepInsta p1(10,20);
+	
+	//copy constructor
+	PrepInsta p2(p1);
+	
+	//another way to call copy constructor 
+	
+	PrepInsta p3=p1;
+	
+	cout<<"p1.x= "<<p1.getX()<<", p1.y= "<<p1.gety()<<endl;
+	cout<<"p2.x= "<<p2.getX()<<", p2.y= "<<p2.gety()<<endl;
+	cout<<"p3.x= "<<p3.getX()<<", p3.y= "<<p3.gety()<<endl;
+}
