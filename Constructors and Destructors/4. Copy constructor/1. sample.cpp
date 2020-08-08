cc7254c841e1967c465170bc503943cabfcbc1aa
Asrasadik <47492644+Asrasadik@users.noreply.github.com>
2020-08-08 11:57:12 +0530

    updated copy constructor

--- Constructors and Destructors/4. Copy constructor/1. sample.cpp
+++ Constructors and Destructors/4. Copy constructor/1. sample.cpp
@@ -5,6 +5,9 @@ class PrepInsta{
 	private:
 		int x,y;
 	public:
+		PrepInsta(){
+			cout<<"Default constructor is called"<<endl;
+		}
 		//parameterised constructor
 		PrepInsta(int x1, int y1){
 			x=x1;
@@ -36,7 +39,12 @@ int main(){
 	
 	PrepInsta p3=p1;
 	
+	PrepInsta p4;
+	p4=p1;/*it is like assignment values are assigned but copy constructor is not called and*/
+	
+	
 	cout<<"p1.x= "<<p1.getX()<<", p1.y= "<<p1.gety()<<endl;
 	cout<<"p2.x= "<<p2.getX()<<", p2.y= "<<p2.gety()<<endl;
 	cout<<"p3.x= "<<p3.getX()<<", p3.y= "<<p3.gety()<<endl;
+	cout<<"p4.x= "<<p4.getX()<<", p4.y= "<<p4.gety()<<endl;
 }
